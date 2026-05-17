c2bd0df99d2ef8d27c7a0ffd2308f3d50e6ae77a
ayush180320 <84795921+ayush180320@users.noreply.github.com>
2026-05-17 08:07:11 +0530

    Create main.ts

---
+++ electron/main.ts
@@ -0,0 +1,12 @@
+import { app, BrowserWindow } from 'electron'
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1800,
+    height: 1000
+  })
+
+  win.loadURL('http://localhost:5173')
+}
+
+app.whenReady().then(createWindow)
